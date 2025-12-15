6e386b26caf44a4a368defe5d9953c09d186bcbf
wertyqssei <st9143918@stud.duikt.edu.ua>
2025-12-15 23:30:41 +0200

    Implement program to find second largest element

---
+++ lb8(5).cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Введiть кiлькiсть елементiв масиву: ";
+    cin >> n;
+
+    if (n < 2) {
+        cout << "Помилка: масив повинен мiстити щонайменше 2 елементи.";
+        return 0;
+    }
+
+    int a[100]; 
+    cout << "Введiть елементи масиву (рiзнi числа):\n";
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int max1, max2;
+
+    if (a[0] > a[1]) {
+        max1 = a[0];
+        max2 = a[1];
+    } else {
+        max1 = a[1];
+        max2 = a[0];
+    }
+
+    for (int i = 2; i < n; i++) {
+        if (a[i] > max1) {
+            max2 = max1;
+            max1 = a[i];
+        } else if (a[i] > max2) {
+            max2 = a[i];
+        }
+    }
+
+    cout << "Другий за величиною елемент масиву: " << max2;
+    return 0;
+}
